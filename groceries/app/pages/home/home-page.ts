12538f785c8beed16dc7165e8657df4c49f97544
thomasgalicia <11601037@student.pxl.be>
2019-09-08 20:20:11 +0200

    GRO-01: Add navigation to 'saved-lists' page

--- groceries/app/pages/home/home-page.ts
+++ groceries/app/pages/home/home-page.ts
@@ -1,9 +1,18 @@
-import { EventData } from "tns-core-modules/ui/page/page";
+import { EventData, Page } from "tns-core-modules/ui/page/page";
 import { Button } from "tns-core-modules/ui/button/button"
 import { Frame } from "tns-core-modules/ui/frame/frame";
 
-export function onBtnNewTap(args: EventData) {
-    let button: Button = args.object as Button;
-    let frame: Frame = button.page.frame;
+let frame: Frame = undefined;
+
+export function onPageLoaded(args: EventData): void {
+    let page: Page = args.object as Page;
+    frame = page.frame;
+}
+
+export function onBtnNewTap(args: EventData): void {
     frame.navigate('pages/new-list/new-list-page');
+}
+
+export function onBtnListsTap(args: EventData): void {
+    frame.navigate('pages/saved-lists/saved-lists-page');
 }
