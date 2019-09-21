f849d875c627605ec09776ba76d9822f19281479
thomasgalicia <11601037@student.pxl.be>
2019-09-21 18:43:05 +0200

    GRO-03: Add arrow back to shopping list page

--- groceries/app/pages/shopping-list/shopping-list-page.ts
+++ groceries/app/pages/shopping-list/shopping-list-page.ts
@@ -1,15 +1,23 @@
 import { NavigatedData, Page } from "tns-core-modules/ui/page/page";
-import { Frame } from "tns-core-modules/ui/frame/frame";
+import { Frame, NavigationEntry } from "tns-core-modules/ui/frame/frame";
 import { ShoppingListViewModel } from "./shopping-list-view-model";
 
 let page: Page = undefined;
 let frame: Frame = undefined;
 let viewModel: ShoppingListViewModel = undefined;
 
-export function onNavigatedTo(args: NavigatedData) {
+export function onNavigatedTo(args: NavigatedData): void{
     let shoppingListId: number = args.context['shoppingListId'] as number;
     viewModel = new ShoppingListViewModel(shoppingListId);
     page = args.object as Page;
     page.bindingContext = viewModel;
     frame = page.frame;
+}
+
+export function onNavBackTap(): void{
+    let navEntry: NavigationEntry = {
+        moduleName: 'pages/home/home-page',
+        clearHistory: true
+    };
+    frame.navigate(navEntry);
 }
