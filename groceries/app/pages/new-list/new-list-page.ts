e28442f6338768b31537718a4cd01f2e78cbcd11
thomasgalicia <11601037@student.pxl.be>
2019-09-21 18:47:24 +0200

    GR0-02: Fix app crash when creating new list by providing context on navigation

--- groceries/app/pages/new-list/new-list-page.ts
+++ groceries/app/pages/new-list/new-list-page.ts
@@ -1,24 +1,29 @@
-import { Frame, EventData, Page } from "tns-core-modules/ui/frame/frame";
+import { Frame, EventData, Page, NavigationEntry } from "tns-core-modules/ui/frame/frame";
 import { NewListViewModel } from "./new-list-viewmodel";
+import { ShoppingList } from "~/models/shoppinglist";
 
 let frame: Frame = undefined;
 let page: Page = undefined;
 let viewModel: NewListViewModel = new NewListViewModel();
 
-export function onPageLoaded(args: EventData): void {
+export function onPageLoaded(args: EventData): void{
     page = args.object as Page;
     page.bindingContext = viewModel;
     frame = page.frame;
 }
 
-export function onNavBackTap(args: EventData): void {
+export function onNavBackTap(args: EventData): void{
     frame.goBack();
 }
 
-export async function onCreateTap(): Promise<void> {
+export async function onCreateTap(): Promise<void>{
   try {
-    let shoppingList = await viewModel.createList();  
-    frame.navigate("pages/shopping-list/shopping-list-page");  
+    let shoppingList: ShoppingList = await viewModel.createList();  
+    let navEntry: NavigationEntry = {
+      context: {'shoppingListId': shoppingList.Id},
+      moduleName: 'pages/shopping-list/shopping-list-page'
+    }
+    frame.navigate(navEntry);  
     
   } catch (error) {
       console.log(error);
